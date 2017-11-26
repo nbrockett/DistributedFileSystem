74a3ce515e92f2efedd03055ad64e5db276f2466
nbrockett <n.brockett@gmail.com>
2017-11-26 10:58:36 +0000

    implemented basic working file server

--- dfs/file_server.py
+++ dfs/file_server.py
@@ -8,30 +8,36 @@ from flask import Response
 from flask import json
 from flask import jsonify
 
+import os
+import shutil
+
 app = Flask(__name__)
+FILE_DIR_ROOT = "./files_root"
 
-
-
-
-# Adding logging functionality
-import logging
-file_handler = logging.FileHandler('fs.log')
-app.logger.addHandler(file_handler)
-app.logger.setLevel(logging.INFO)
+#
+# # Adding logging functionality
+# import logging
+# file_handler = logging.FileHandler('fs.log')
+# app.logger.addHandler(file_handler)
+# app.logger.setLevel(logging.INFO)
 
 
 @app.route('/', methods=['GET'])
 def get():
 
-    # example for logging
-    app.logger.info('informing')
-    app.logger.warning('warning')
-    app.logger.error('error')
+    # # example for logging
+    # app.logger.info('informing')
+    # app.logger.warning('warning')
+    # app.logger.error('error')
+
+    # return list of all entries in root_dir of file server
+    files = os.listdir(file_server.root_dir)
 
     data = {
-        'hello': 'world',
-        'number': 3
+        'files': files,
     }
+
+
     # js = json.dumps(data)
     # resp = Response(js, status=200, mimetype='application/json')
     resp = jsonify(data)
@@ -42,25 +48,43 @@ def get():
 @app.route('/', methods=['POST'])
 def post():
 
-    if request.headers['Content-Type'] == 'text/plain':
-        return "Text Message: " + request.data
+    # if request.headers['Content-Type'] == 'text/plain':
+    #     return "Text Message: " + request.data
 
-    elif request.headers['Content-Type'] == 'application/json':
-        return "JSON Message: " + json.dumps(request.json)
+    if request.headers['Content-Type'] == 'application/json':
 
-    elif request.headers['Content-Type'] == 'application/octet-stream':
-        f = open('./binary', 'wb')
-        f.write(request.data)
-        f.close()
-        return "Binary message written!"
+        data = request.json
+
+        file_name = data['file_name']
+        file_content = data['content']
+
+
+        file_path = os.path.join(file_server.root_dir, file_name)
+        f = open(file_path, 'w')
+        f.write(str(file_content))
+
+
+        print("File {0} written to file server".format(file_path))
+
     else:
-        return "415 Unsupported Media Type ;)"
-
+        raise NotImplementedError
 
 class FileServer:
 
-    def __init__(self):
-        self.files = None
+    def __init__(self, root_dir):
+
+        self.root_dir = root_dir
+        self.reset()
+
+    def reset(self):
+        """ reset root folder directory"""
+
+        # if path exists remove directory. Create new root dir
+        if os.path.exists(self.root_dir):
+            shutil.rmtree(self.root_dir)
+            os.makedirs(self.root_dir)
+        else:
+            os.makedirs(self.root_dir)
 
 
 if __name__ == '__main__':
@@ -75,11 +99,14 @@ if __name__ == '__main__':
     parser.add_argument(
         '--port',
         type=int,
-        default=8000,
+        default=8001,
         help='port of server.'
     )
 
     FLAGS, unparsed = parser.parse_known_args()
+
+    # setup file server
+    file_server = FileServer(FILE_DIR_ROOT)
 
     # run application with set flags
     app.run(host=FLAGS.host, port=FLAGS.port)
